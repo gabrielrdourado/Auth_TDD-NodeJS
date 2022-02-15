f8924744a0f9743e70e1a79702e18d4956686496
Gabriel Dourado <gabrielribeirodourado@hotmail.com>
2022-02-15 14:25:09 -0300

    gitignore and structure create

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "gftechs.database.windows.net",
+  username: "gftechsdbadmin",
+  password: "T@4b33d6fg",
+  database: "authtdd",
+  dialect: "mssql",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+const routes = require("express").Router();
+
+//Rotas
+
+module.exports = routes;
